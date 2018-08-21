0946fd9d394449a822db1907da85e2bf9cfe093f
Vilde Vevatne <v-viveva@microsoft.com>
2018-08-21 13:20:22 -0700

    Refactor with dependency injection

--- design-patterns/design-patterns/Program.cs
+++ design-patterns/design-patterns/Program.cs
@@ -10,5 +10,30 @@ namespace design_patterns
         static void Main(string[] args)
         {
         }
+        public class Dependency
+        {
+            public void DoSomething()
+            {
+                Console.WriteLine("Did something");
+            }
+
+            public void DoSomethingElse()
+            {
+                Console.WriteLine("Did something else");
+            }
+        }
+
+        public class Consumer
+        {
+            public void DoSomething(Dependency d)
+            {
+                d.DoSomething();
+            }
+
+            public void DoSomethingElse(Dependency d)
+            {
+                d.DoSomethingElse();
+            }
+        }
     }
 }
